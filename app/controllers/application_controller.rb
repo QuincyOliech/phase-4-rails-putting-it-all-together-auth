358d069270a6e0442f406280184c2d9d9a8d5914
Linet Oliech <linetaoliech@gmail.com>
2023-04-01 00:15:10 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_error
+
+
+  private
+
+  def render_not_found_error
+    render json: {errors: "#{controller_name.classify} not found"}, status: 422
+  end
+
+  def render_invalid_error invalid 
+    render json: {errors: invalid.record.errors.full_messages}, status: 422
+  end
 
 end
